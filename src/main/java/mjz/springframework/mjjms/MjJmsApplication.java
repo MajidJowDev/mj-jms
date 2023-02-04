c4fa4ec7d5118aebeb81db552424f29011fb6a20
MajidJowDev <majid.jow@gmail.com>
2023-02-04 10:51:58 +0330

    29- 5. Embedded Server Configuration

--- src/main/java/mjz/springframework/mjjms/MjJmsApplication.java
+++ src/main/java/mjz/springframework/mjjms/MjJmsApplication.java
@@ -1,12 +1,26 @@
 package mjz.springframework.mjjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MjJmsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        //just for test purpose, most of the times the ActiveMQ started outside of spring
+        //setting up a minimal ActiveMQ Server
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0"));// will give us error "Acceptor with id 0 already registered", but ignore it
+
+        server.start();
+
         SpringApplication.run(MjJmsApplication.class, args);
     }
 
